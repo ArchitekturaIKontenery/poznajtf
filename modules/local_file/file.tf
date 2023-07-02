14530ba926f33106a599b3938e8e1929adfe9bf0
Kamil Mrzygłód <kamil@thecloudtheory.com>
2023-07-02 11:49:42 +0200

    Mała zmiana dla modułu

--- modules/local_file/file.tf
+++ modules/local_file/file.tf
@@ -1,13 +1,13 @@
 resource "local_file" "local_file" {
   count = var.is_sensitive ? 0 : 1
 
-  content  = "Hello, World!"
-  filename = "local_file.txt"
+  content  = var.content
+  filename = var.filename
 }
 
 resource "local_sensitive_file" "local_sensitive_file" {
   count = var.is_sensitive ? 1 : 0
 
-  content  = "Hello, World!"
-  filename = "local_sensitive_file.txt"
+  content  = var.content
+  filename = var.filename
 }
--- modules/local_file/variables.tf
+++ modules/local_file/variables.tf
@@ -2,3 +2,13 @@ variable "is_sensitive" {
   type    = bool
   default = false
 }
+
+variable "content" {
+  type    = string
+  default = "This is a local file"
+}
+
+variable "filename" {
+  type    = string
+  default = "local_file.txt"
+}
